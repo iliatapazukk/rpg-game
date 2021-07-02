42c3e682b81270e4629391b2ac456ccc53b3ab82
iliatapazukk <mozesinambient@gmail.com>
2021-07-03 00:45:29 +0300

    Canvas

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './index.scss';
+import gamePerson from './assets/Male-5-Walk.png';
 
-console.log('!!! init');
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+const spriteW = 48;
+const spriteH = 48;
+const shots = 3;
+let cycle = 0;
+
+const img = document.createElement('img');
+img.src = gamePerson;
+
+img.addEventListener('load', () => {
+  setInterval(() => {
+    cycle =(cycle + 1) % shots;
+    ctx.clearRect(0, 0, 600, 600)
+    ctx.drawImage(img, cycle * spriteW, 0, spriteW, spriteH, 0, 0, 100, 100)
+  }, 120)
+})
+
+// let xy = 0
+// setInterval(() => {
+//   ctx.clearRect(0, 0, 600,600);
+//   xy +=1;
+//   ctx.fillStyle = 'red';
+//   ctx.fillRect(xy, xy, 100, 100)
+// }, 10)
+
+// ctx.beginPath();
+// ctx.moveTo(50,50)
+// ctx.lineTo(550, 50)
+// ctx.lineTo(50, 100)
+// ctx.lineTo(550, 100)
+// ctx.stroke()
+
