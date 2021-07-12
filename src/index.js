8946bda8a4004e347a4a05b3e0c65a0a1e0cc027
iliatapazukk <mozesinambient@gmail.com>
2021-07-12 05:31:31 +0300

    requestAnimationFrame

--- src/index.js
+++ src/index.js
@@ -64,63 +64,40 @@ document.addEventListener('keyup', keyUpHandler);
 const img = document.createElement('img');
 img.src = gamePerson;
 
+function walk(timestamp) {
+  console.log('!!! timestamp:', timestamp)
+  arrKeyPressed ? (cycle = (cycle + 1) % shots) : null;
+  if (bottomPressed === true) {
+    pY += 10;
+    if (pY > canvasH - spriteH) {
+      pY = canvasH - spriteH;
+    }
+    direction = 0;
+  } else if (upPressed === true) {
+    pY -= 10;
+    if (pY < 0) {
+      pY = 0;
+    }
+    direction = spriteH * 3;
+  } else if (leftPressed === true) {
+    pX -= 10;
+    if (pX < 0) {
+      pX = 0;
+    }
+    direction = spriteW;
+  } else if (rightPressed === true) {
+    pX += 10;
+    if (pX > canvasW - spriteW) {
+      pX = canvasW - spriteW;
+    }
+    direction = spriteW * 2;
+  }
+
+  ctx.clearRect(0, 0, rect, rect);
+  ctx.drawImage(img, cycle * spriteW, direction, spriteW, spriteH, pX, pY, 48, 48);
+  window.requestAnimationFrame(walk);
+}
+
 img.addEventListener('load', () => {
-  setInterval(() => {
-    arrKeyPressed ? (cycle = (cycle + 1) % shots) : null;
-    if (bottomPressed === true) {
-      pY += 10;
-      if (pY > canvasH - spriteH) {
-        pY = canvasH - spriteH;
-      }
-      direction = 0;
-    } else if (upPressed === true) {
-      pY -= 10;
-      if (pY < 0) {
-        pY = 0;
-      }
-      direction = spriteH * 3;
-    } else if (leftPressed === true) {
-      pX -= 10;
-      if (pX < 0) {
-        pX = 0;
-      }
-      direction = spriteW;
-    } else if (rightPressed === true) {
-      pX += 10;
-      if (pX > canvasW - spriteW) {
-        pX = canvasW - spriteW;
-      }
-      direction = spriteW * 2;
-    }
-
-    ctx.clearRect(0, 0, rect, rect);
-
-    // background temp
-    let radius = 0;
-    let angle = 0;
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = '#0096FF';
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, canvas.height / 2);
-    for (let n = 0; n < rect; n++) {
-      radius += 0.75;
-      angle += (Math.PI * 2) / 50;
-      const x = canvas.width / 2 + radius * Math.cos(angle);
-      const y = canvas.height / 2 + radius * Math.sin(angle);
-      ctx.lineTo(x, y);
-    }
-
-    ctx.stroke();
-
-    ctx.drawImage(img, cycle * spriteW, direction, spriteW, spriteH, pX, pY, 48, 48);
-
-    console.log('!!! position:', pX);
-  }, 120);
+  requestAnimationFrame(walk);
 });
-
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(550, 50);
-ctx.lineTo(50, 100);
-ctx.lineTo(550, 100);
-ctx.stroke();
