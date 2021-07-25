3e3d23da159564ea6bef552209bc223265bec693
iliatapazukk <74911110+iliatapazukk@users.noreply.github.com>
2021-07-25 10:34:58 +0300

    add Camera, add & move Player
    * Prepare and Create Camera
    
    * add map
    
    * Add Player
    
    * Move Player

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,20 +10,27 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-      'quotes': 0,
-      'no-nested-ternary': 0,
-      'indent': 0,
-      'default-case': 0,
-      'no-unused-expressions': 0,
-      'no-console': 0,
-      'no-plusplus': 0,
-      'no-commented-out-code': 0,
-      'no-unused-vars': 0, // включить в дальнейшем
-      'no-restricted-syntax': 0, // включить в дальнейшем
-      'guard-for-in': 0, // включить в дальнейшем
-      'prefer-const': 0, // включить в дальнейшем
-      'object-shorthand': 0, // включить в дальнейшем
-      'func-names': 0, // включить в дальнейшем
-      'object-curly-newline': 0, // включить в дальнейшем
+    'max-len': 0,
+    quotes: 0,
+    'arrow-body-style': 0,
+    'camelcase': 0,
+    'no-nested-ternary': 0,
+    'no-param-reassign': 0,
+    indent: 0,
+    'default-case': 0,
+    'no-unused-expressions': 0,
+    'no-console': 0,
+    'no-plusplus': 0,
+    'no-commented-out-code': 0,
+    'prefer-destructuring': 0,
+    'prefer-arrow-callback': 0,
+    'import/prefer-default-export': 0,
+    'no-unused-vars': 0, // включить в дальнейшем
+    'no-restricted-syntax': 0, // включить в дальнейшем
+    'guard-for-in': 0, // включить в дальнейшем
+    'prefer-const': 0, // включить в дальнейшем
+    'object-shorthand': 0, // включить в дальнейшем
+    'func-names': 0, // включить в дальнейшем
+    'object-curly-newline': 0, // включить в дальнейшем
   },
 };
--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -2,16 +2,23 @@ import ClientEngine from './ClientEngine';
 import ClientWorld from './ClientWorld';
 import sprites from '../configs/sprites';
 import levelCfg from '../configs/world.json';
+import gameObjects from '../configs/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
     Object.assign(this, {
       cfg,
+      gameObjects,
+      player: null,
     });
 
     this.engine = this.createEngine();
-    this.world = this.createWorld();
+    this.map = this.createWorld();
     this.initEngine();
+  }
+
+  setPlayer(player) {
+    this.player = player;
   }
 
   createEngine() {
@@ -24,10 +31,65 @@ class ClientGame {
 
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
+      this.map.init();
       this.engine.on('render', (_, time) => {
-        this.world.init();
+        this.map.render(time);
       });
       this.engine.start();
+      this.initKeys();
+      // отключение скролла клавишами при передвижении игрока
+      window.addEventListener(
+        'keydown',
+        function (e) {
+          if (['Space', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].indexOf(e.code) > -1) {
+            e.preventDefault();
+          }
+        },
+        false,
+      );
+    });
+  }
+
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (keydown) => {
+        console.log('!!! ArrowLeft:');
+        if (keydown) {
+          this.player.moveByCellCoord(-1, 0, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          });
+        }
+      },
+    });
+    this.engine.input.onKey({
+      ArrowRight: (keydown) => {
+        console.log('!!! ArrowRight:');
+        if (keydown) {
+          this.player.moveByCellCoord(+1, 0, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          });
+        }
+      },
+    });
+    this.engine.input.onKey({
+      ArrowUp: (keydown) => {
+        console.log('!!! ArrowTop:');
+        if (keydown) {
+          this.player.moveByCellCoord(0, -1, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          });
+        }
+      },
+    });
+    this.engine.input.onKey({
+      ArrowDown: (keydown) => {
+        console.log('!!! ArrowBottom:');
+        if (keydown) {
+          this.player.moveByCellCoord(0, +1, (cell) => {
+            return cell.findObjectsByType('grass').length;
+          });
+        }
+      },
     });
   }
 
